5a93a060407cf13cb1d2e75dba85af6d1bd80af4
Matt Joiner <anacrolix@gmail.com>
2010-10-11 10:05:06 +1100

    Several usability changes, and package resolving Resolve package changes to prevent serious damage, allow override with -f. Try to grab lock at start, to prevent lack of privileges and exclusion later. Retry the cache commit on failure, this gives the user the chance to fix problems without losing his package markings.

--- apt-downgrade.py
+++ apt-downgrade.py
@@ -23,13 +23,28 @@ parser.add_option("-u", "--update", action="store_true", default=False,
         help="update the package cache first")
 parser.add_option("-y", "--yes", action="store_true", default=False,
         help="assume yes to all questions for which yes is the default")
+parser.add_option("-f", "--force", action="store_true", default=False,
+        help="force the requested changes, don't resolve problems")
 opts, args = parser.parse_args()
 
-import apt, apt.progress
+import apt, apt.progress, apt_pkg
+
+# try to grab the system lock briefly, so we're not caught without it later
+try:
+    with apt_pkg.SystemLock():
+        pass
+except SystemError as exc:
+    print exc
+    raise SystemExit(100) # see apt-get(8)
+
+print "Loading package cache..."
 cache = apt.Cache()
+
 if opts.update:
     print "Updating package cache..."
     cache.update(apt.progress.TextFetchProgress())
+
+print "Searching for installed packages that are no longer available..."
 for pkg in cache:
     if pkg.installed and not pkg.candidate.downloadable:
         cand = None
@@ -39,7 +54,7 @@ for pkg in cache:
         #print pkg.installed, "->", cand
         if cand != None:
             if user_proceed(
-                    "Force '%s' version\n\tfrom %s\n\tto   %s (source: %s) [y]: "
+                    "Force '%s' version\n  from %s\n  to   %s (%s) [y]? "
                     % ( pkg.name,
                         pkg.installed.version,
                         cand.version,
@@ -47,17 +62,32 @@ for pkg in cache:
                 pkg.candidate = cand
                 pkg.mark_install(autoFix=False, fromUser=False)
             #pkg.mark_upgrade()
-print "The following changes will be made..."
+
+print "Resolving package problems (pass -f to disable this step)..."
+apt.cache.ProblemResolver(cache).resolve()
+
 if cache.get_changes():
+    print "The following changes will be made..."
     for pkg in cache.get_changes():
-        print "\t%s %s => %s (source: %s)" % (
+        print "  %s %s => %s (%s)" % (
                 pkg.name,
                 pkg.installed and pkg.installed.version,
                 pkg.candidate.version,
                 pick_origin(pkg.candidate).archive)
-    print "%i bytes will be downloaded" % (cache.required_download)
-    if user_proceed("Do you wish to make the changes above? [n]", default=False):
-        cache.commit(apt.progress.TextFetchProgress(), apt.progress.InstallProgress())
+    print "%.1f MB will be downloaded" % (cache.required_download / 1e6)
+    if user_proceed("Do you wish to make the changes above? [n]? ", default=False):
+        while True:
+            try:
+                cache.commit(
+                        apt.progress.TextFetchProgress(),
+                        apt.progress.InstallProgress())
+            except SystemError as exc:
+                print exc
+                if not user_proceed("An error occurred, try again? [y]? "):
+                    raise SystemExit(100) # see apt-get(8)
+                continue
+            else:
+                break
     else:
         print "User cancelled."
 else:
