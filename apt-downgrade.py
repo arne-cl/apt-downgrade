1ca594d3a6947ac9bc8c336c57aa527f08eee26d
Arne Neumann <github@arne.cl>
2014-09-16 11:56:09 +0200

    refactoring: put script code in "if name == '__main__'" part

--- apt-downgrade.py
+++ apt-downgrade.py
@@ -12,6 +12,7 @@ def user_proceed(prompt, default=True):
         if "no".startswith(rin.lower()):
             return False
 
+
 def pick_origin(pkgver):
     from sys import stderr
     goodors = filter(lambda x: x.archive != "now", pkgver.origins)
@@ -24,83 +25,86 @@ def pick_origin(pkgver):
     else:
         assert len(pkgver.origins) == 1, pkgver.origins
         return pkgver.origins[0]
-from optparse import OptionParser
-parser = OptionParser()
-parser.add_option("-u", "--update", action="store_true", default=False,
-        help="update the package cache first")
-parser.add_option("-y", "--yes", action="store_true", default=False,
-        help="assume yes to all questions for which yes is the default")
-parser.add_option("-f", "--force", action="store_true", default=False,
-        help="force the requested changes, don't resolve problems")
-opts, args = parser.parse_args()
 
-import apt, apt.progress, apt_pkg
 
-# try to grab the system lock briefly, so we're not caught without it later
-try:
-    with apt_pkg.SystemLock():
-        pass
-except SystemError as exc:
-    print exc
-    raise SystemExit(100) # see apt-get(8)
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-u", "--update", action="store_true", default=False,
+            help="update the package cache first")
+    parser.add_option("-y", "--yes", action="store_true", default=False,
+            help="assume yes to all questions for which yes is the default")
+    parser.add_option("-f", "--force", action="store_true", default=False,
+            help="force the requested changes, don't resolve problems")
+    opts, args = parser.parse_args()
 
-print "Loading package cache..."
-cache = apt.Cache()
+    import apt, apt.progress, apt_pkg
 
-if opts.update:
-    print "Updating package cache..."
-    cache.update(apt.progress.TextFetchProgress())
+    # try to grab the system lock briefly, so we're not caught without it later
+    try:
+        with apt_pkg.SystemLock():
+            pass
+    except SystemError as exc:
+        print exc
+        raise SystemExit(100) # see apt-get(8)
 
-print "Searching for installed packages that are no longer available..."
-for pkg in cache:
-    if pkg.installed and not pkg.candidate.downloadable:
-        cand = None
-        for ver in pkg.versions:
-            if ver.downloadable and ver > cand:
-                cand = ver
-        #print pkg.installed, "->", cand
-        if cand is None:
-            if user_proceed(
-                    "Remove '%s' version %s [y]?" % (
-                    pkg.name, pkg.installed.version)):
-                #pkg.candidate = None
-                pkg.mark_delete(auto_fix=False, purge=False)
-                assert pkg.marked_delete
-        if cand != None:
-            if user_proceed(
-                    "Force '%s' version\n  from %s\n  to   %s (%s) [y]? "
-                    % ( pkg.name,
-                        pkg.installed.version,
-                        cand.version,
-                        pick_origin(cand).archive)):
-                pkg.candidate = cand
-                pkg.mark_install(auto_fix=False, from_user=False)
-            #pkg.mark_upgrade()
+    print "Loading package cache..."
+    cache = apt.Cache()
 
-print "Resolving package problems (pass -f to disable this step)..."
-apt.cache.ProblemResolver(cache).resolve()
+    if opts.update:
+        print "Updating package cache..."
+        cache.update(apt.progress.TextFetchProgress())
 
-if cache.get_changes():
-    print "The following changes will be made..."
-    for pkg in cache.get_changes():
-        print "  %s %s => %s (%s)" % (
-                pkg.name,
-                pkg.installed and pkg.installed.version,
-                None if pkg.marked_delete else pkg.candidate.version,
-                pick_origin(pkg.candidate).archive)
-    print "%.1f MB will be downloaded" % (cache.required_download / 1e6)
-    if user_proceed("Do you wish to make the changes above? [n]? ", default=False):
-        while True:
-            try:
-                cache.commit()
-            except SystemError as exc:
-                print exc
-                if not user_proceed("An error occurred, try again? [y]? "):
-                    raise SystemExit(100) # see apt-get(8)
-                continue
-            else:
-                break
+    print "Searching for installed packages that are no longer available..."
+    for pkg in cache:
+        if pkg.installed and not pkg.candidate.downloadable:
+            cand = None
+            for ver in pkg.versions:
+                if ver.downloadable and ver > cand:
+                    cand = ver
+            #print pkg.installed, "->", cand
+            if cand is None:
+                if user_proceed(
+                        "Remove '%s' version %s [y]?" % (
+                        pkg.name, pkg.installed.version)):
+                    #pkg.candidate = None
+                    pkg.mark_delete(auto_fix=False, purge=False)
+                    assert pkg.marked_delete
+            if cand != None:
+                if user_proceed(
+                        "Force '%s' version\n  from %s\n  to   %s (%s) [y]? "
+                        % ( pkg.name,
+                            pkg.installed.version,
+                            cand.version,
+                            pick_origin(cand).archive)):
+                    pkg.candidate = cand
+                    pkg.mark_install(auto_fix=False, from_user=False)
+                #pkg.mark_upgrade()
+
+    print "Resolving package problems (pass -f to disable this step)..."
+    apt.cache.ProblemResolver(cache).resolve()
+
+    if cache.get_changes():
+        print "The following changes will be made..."
+        for pkg in cache.get_changes():
+            print "  %s %s => %s (%s)" % (
+                    pkg.name,
+                    pkg.installed and pkg.installed.version,
+                    None if pkg.marked_delete else pkg.candidate.version,
+                    pick_origin(pkg.candidate).archive)
+        print "%.1f MB will be downloaded" % (cache.required_download / 1e6)
+        if user_proceed("Do you wish to make the changes above? [n]? ", default=False):
+            while True:
+                try:
+                    cache.commit()
+                except SystemError as exc:
+                    print exc
+                    if not user_proceed("An error occurred, try again? [y]? "):
+                        raise SystemExit(100) # see apt-get(8)
+                    continue
+                else:
+                    break
+        else:
+            print "User cancelled."
     else:
-        print "User cancelled."
-else:
-    print "There are no changes to be made."
+        print "There are no changes to be made."
